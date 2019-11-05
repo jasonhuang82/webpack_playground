3f2d5ce454d289c4c515514aa5ac4696c1ed7934
jason <jasonhuang702@gmail.com>
2019-11-06 02:08:18 +0800

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
 import App from './pages/app/app';
-import React from 'react'
-import ReactDom from 'react-dom'
 
 const root = document.getElementById('root');
-ReactDom.render(<App/>, root);
-
+ReactDom.render(<App />, root);
