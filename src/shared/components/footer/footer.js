ed1b10e916067a97dcf628646f807eccf781fce6
JasonHuang <jasonhuang@catchplay.com>
2019-11-08 14:42:30 +0800

    add shared footer

---
+++ src/shared/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+            Footer
+        </footer>
+    );
+}
+
+
+export default Footer;
